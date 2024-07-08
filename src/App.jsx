6daf9fa53e767a7ac14f3e4fa6392184de28fa95
chibuike <chrisroyalty001@gmail.com>
2024-07-08 17:34:27 +0100

    useEffect with fake store api

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
 import "./App.css";
 import Button from "./Button";
-import Modal from "./Modal";
-import { useState } from "react";
-import Header from "./Header";
-import MyHeader from "./MyHeader";
+// import Modal from "./Modal";
+import { useState, useEffect } from "react";
+// import Header from "./Header";
+// import MyHeader from "./MyHeader";
 // import HeaderComponent from "./HeaderComponent";
 // import MenuDisplay from "./MenuDisplay";
 function App() {
-  const [toggleModal, setToggleModal] = useState(false);
-  const handleClick = () => {
-    setToggleModal(true);
-  };
+  const [products, setProducts] = useState([]);
+  const api = "https://fakestoreapi.com/products";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(api);
+
+      const data = await response.json();
+      console.log(data);
+
+      setProducts(data);
+    };
+    fetchData();
+  }, []);
 
   return (
     <>
-      {/* <HeaderComponent /> */}
-      {/* <Header /> */}
-      <MyHeader />
-      <div className="">
-        <h1 className="text-4xl">Conditional Rendering</h1>
-      </div>
-      <main className="h-screen flex justify-center items-center">
-        <Button
-          text={"Open Modal"}
-          bgColor={"bg-blue-600"}
-          onclick={handleClick}
-        />
-        {/* using double ampersand */}
-        {/* {toggleModal && <Modal />} */}
-        {toggleModal ? <Modal setToggleModal={setToggleModal} /> : null}
-        {/* Using the ternary operator */}
+      <h1 className="text-4xl">Products</h1>
+      <main className="flex justify-center items-center h-screen flex-wrap gap-5">
+        {products.map((product, index) => (
+          <figure
+            key={index}
+            className="w-1/4 h-96 border-[1px] border-black/60"
+          >
+            <img
+              src={product.image}
+              alt="product image"
+              className="w-full h-60"
+            />
+            <figcaption className="space-y-3 grid justify-items-center">
+              <strong className="text-sm text-center">{product.title}</strong>
+              <br />
+              <strong className="text-sm text-center">{product.price}</strong>
+            </figcaption>
+          </figure>
+        ))}
       </main>
     </>
   );
